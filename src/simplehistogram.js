ad75f9001f81d73bd1ec2bcbee6381e2578c287a
Rainer Simon <rainer.simon@ait.ac.at>
2015-09-28 14:54:07 +0200

    API updates

--- src/simplehistogram.js
+++ src/simplehistogram.js
@@ -4,19 +4,33 @@ var SimpleHistogram = function(container, opts) {
         return (opts) ? ((opts[key]) ? opts[key] : defaultVal) : defaultVal;
       },
 
-      NUMBER_OF_BINS = getOpt('bins', 20),
-
       ALIGN = getOpt('align', 'bottom'),
 
       ORIENTATION = (ALIGN === 'bottom') ? 'landscape' : 'portrait',
 
-      BIN_WIDTH = (ORIENTATION === 'landscape') ?
-        Math.floor(jQuery(container).innerWidth() / NUMBER_OF_BINS) :
-        Math.floor(jQuery(container).innerHeight() / NUMBER_OF_BINS),
+      NUMBER_OF_BINS = getOpt('bins', 20),
+
+      LOG_SCALE = getOpt('logScale', false),
+
+      TOOLTIP_FN = getOpt('tooltip', false),
+
+      computeBinWidth = function() {
+        var binWidth = (ORIENTATION === 'landscape') ?
+          Math.floor(jQuery(container).innerWidth() / NUMBER_OF_BINS) :
+          Math.floor(jQuery(container).innerHeight() / NUMBER_OF_BINS);
+
+        return binWidth;
+      },
+
+      BIN_WIDTH = computeBinWidth(),
 
       data = getOpt('data', []),
 
       containerEl = jQuery(container),
+
+      eventHandlers = {},
+
+      stepSize,
 
       /** Calculates the sum of all values within the specified interval [fromX, toX) **/
       sum = function(data, fromX, toX) {
@@ -35,7 +49,7 @@ var SimpleHistogram = function(container, opts) {
 
       /** Function to re-bin the histogram **/
       resample = function(data, numberOfBins) {
-        var startX, endX, runningX, stepSize, i,
+        var startX, endX, runningX, i,
             bins = [];
 
         if (data.length > 0) {
@@ -53,31 +67,76 @@ var SimpleHistogram = function(container, opts) {
             });
           }
 
-          return { stepSize: stepSize, bins: bins };
+          return bins;
         }
       },
 
       /** Renders the histogram into the parent DOM element **/
-      render = function(data) {
-        var resampled = resample(data, NUMBER_OF_BINS),
-            maxY = Math.max.apply(null, jQuery.map(resampled.bins, function(val) { return val.y; })),
+      render = function() {
+        var bins = resample(data, NUMBER_OF_BINS),
+            maxY = Math.max.apply(null, jQuery.map(bins, function(val) { return val.y; })),
             fixedDimension = (ORIENTATION === 'landscape') ? 'width' : 'height',
             varDimension = (ORIENTATION === 'landscape') ? 'height' : 'width';
 
+        if (LOG_SCALE)
+          maxY = Math.log(maxY);
+
         containerEl.empty();
-        jQuery.each(resampled.bins, function(idx, bin) {
-          var len = bin.y / maxY * 100,
+        jQuery.each(bins, function(idx, bin) {
+          var len = LOG_SCALE ? Math.log(bin.y + 1) / maxY * 100 : bin.y / maxY * 100,
+
+              title = TOOLTIP_FN ? TOOLTIP_FN(bin.x, bin.x + stepSize, bin.y) : bin.y,
+
               html = '<div class="histogram column ' + ALIGN + ' ' + ORIENTATION + '" style="' +
-                       fixedDimension + ':' + BIN_WIDTH + 'px" data-x="' + bin.x + '">' +
+                       fixedDimension + ':' + BIN_WIDTH + 'px" data-x="' + bin.x + '" data-y="'  +
+                       bin.y + '" title="' + title + '">' +
                        '<div class="histogram bar" style="' + varDimension + ': ' + len + '%">' +
                        '</div>' +
                      '</div>';
 
           containerEl.append(html);
         });
+      },
+
+      on = function(eventName, handler) {
+        var handlers = eventHandlers[eventName];
+        if (handlers)
+          handlers.push(handler);
+        else
+          eventHandlers[eventName] = [ handler ];
+      },
+
+      triggerEvent = function(handlers, e) {
+        if (handlers) {
+          var targetColumn = jQuery(e.target).closest('.column'),
+              fromX = targetColumn.data('x'),
+              toX = fromX + stepSize,
+              y = targetColumn.data('y');
+
+          jQuery.each(handlers, function(idx, h) {
+            h(fromX, toX, y, e);
+          });
+        }
       };
 
-  if (data)
-    render(data);
+  // Listen to window resize events
+  jQuery(window).on('resize', function() {
+    BIN_WIDTH = computeBinWidth();
+    render();
+  });
+
+  // Listen to mouse events on columns
+  containerEl.on('mouseenter', '.column', function(e) {
+    triggerEvent(eventHandlers.enterbar, e);
+  });
+
+  containerEl.on('click', '.column', function(e) {
+    triggerEvent(eventHandlers.click, e);
+  });
+
+  render();
+
+  // Export public methods
+  this.on = on;
 
 };
